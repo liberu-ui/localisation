8d6a350876a465d10351338ec8e909dca491841a
Mihai <mihai.ocneanu@gmail.com>
2021-12-17 17:05:17 +0200

    wip

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,12 +1,8 @@
-import Vue from 'vue';
 import App from '@enso-ui/ui/src/core/app';
 import MissingKey from './components/navbar/MissingKeys.vue';
 import KeyCollector from './components/settings/KeyCollector.vue';
 import LanguageSelector from './components/settings/LanguageSelector.vue';
 
-Vue.component('navbar-missing-key', MissingKey);
-Vue.component('setting-key-collector', KeyCollector);
-Vue.component('setting-language-selector', LanguageSelector);
-App.registerNavbarItem('navbar-missing-key', 100);
-App.registerSettingsItem('setting-language-selector', 100);
-App.registerSettingsItem('setting-key-collector', 600);
+App.registerNavbarItem('navbar-missing-key', MissingKey, 100);
+App.registerSettingsItem('setting-language-selector', LanguageSelector, 100);
+App.registerSettingsItem('setting-key-collector', KeyCollector, 600);
